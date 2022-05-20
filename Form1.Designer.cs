aee695494c4e2b56f95f810d7a6458375faf3d3d
Maxime Declerck <90453026+UnicornFishy@users.noreply.github.com>
2022-05-20 14:05:12 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,326 @@
+﻿namespace HangManGame
+{
+    partial class a2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LoadBt = new System.Windows.Forms.Button();
+            this.HideBt = new System.Windows.Forms.Button();
+            this.CheckBt = new System.Windows.Forms.Button();
+            this.Hiddentxt1 = new System.Windows.Forms.TextBox();
+            this.Hiddentxt2 = new System.Windows.Forms.TextBox();
+            this.Hiddentxt3 = new System.Windows.Forms.TextBox();
+            this.Hiddentxt4 = new System.Windows.Forms.TextBox();
+            this.Hiddentxt5 = new System.Windows.Forms.TextBox();
+            this.AnswerBox1 = new System.Windows.Forms.TextBox();
+            this.AnswerBox2 = new System.Windows.Forms.TextBox();
+            this.AnswerBox3 = new System.Windows.Forms.TextBox();
+            this.AnswerBox4 = new System.Windows.Forms.TextBox();
+            this.AnswerBox5 = new System.Windows.Forms.TextBox();
+            this.TopicMenu = new System.Windows.Forms.ComboBox();
+            this.Lbl1 = new System.Windows.Forms.Label();
+            this.a1 = new System.Windows.Forms.Label();
+            this.aa2 = new System.Windows.Forms.Label();
+            this.a3 = new System.Windows.Forms.Label();
+            this.a4 = new System.Windows.Forms.Label();
+            this.a5 = new System.Windows.Forms.Label();
+            this.WinLbl = new System.Windows.Forms.Label();
+            this.TopicLbl = new System.Windows.Forms.Label();
+            this.amountCorrectLbl = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // LoadBt
+            // 
+            this.LoadBt.Location = new System.Drawing.Point(352, 47);
+            this.LoadBt.Name = "LoadBt";
+            this.LoadBt.Size = new System.Drawing.Size(75, 23);
+            this.LoadBt.TabIndex = 0;
+            this.LoadBt.Text = "Load word";
+            this.LoadBt.UseVisualStyleBackColor = true;
+            this.LoadBt.Click += new System.EventHandler(this.LoadBt_Click);
+            // 
+            // HideBt
+            // 
+            this.HideBt.Location = new System.Drawing.Point(577, 106);
+            this.HideBt.Name = "HideBt";
+            this.HideBt.Size = new System.Drawing.Size(75, 23);
+            this.HideBt.TabIndex = 1;
+            this.HideBt.Text = "Show hint";
+            this.HideBt.UseVisualStyleBackColor = true;
+            this.HideBt.Click += new System.EventHandler(this.HideBt_Click);
+            // 
+            // CheckBt
+            // 
+            this.CheckBt.Location = new System.Drawing.Point(577, 146);
+            this.CheckBt.Name = "CheckBt";
+            this.CheckBt.Size = new System.Drawing.Size(75, 23);
+            this.CheckBt.TabIndex = 2;
+            this.CheckBt.Text = "Check word";
+            this.CheckBt.UseVisualStyleBackColor = true;
+            this.CheckBt.Click += new System.EventHandler(this.CheckBt_Click);
+            // 
+            // Hiddentxt1
+            // 
+            this.Hiddentxt1.Location = new System.Drawing.Point(9, 106);
+            this.Hiddentxt1.Name = "Hiddentxt1";
+            this.Hiddentxt1.ReadOnly = true;
+            this.Hiddentxt1.Size = new System.Drawing.Size(100, 20);
+            this.Hiddentxt1.TabIndex = 3;
+            this.Hiddentxt1.Visible = false;
+            // 
+            // Hiddentxt2
+            // 
+            this.Hiddentxt2.Location = new System.Drawing.Point(115, 106);
+            this.Hiddentxt2.Name = "Hiddentxt2";
+            this.Hiddentxt2.ReadOnly = true;
+            this.Hiddentxt2.Size = new System.Drawing.Size(100, 20);
+            this.Hiddentxt2.TabIndex = 4;
+            this.Hiddentxt2.Visible = false;
+            // 
+            // Hiddentxt3
+            // 
+            this.Hiddentxt3.Location = new System.Drawing.Point(221, 106);
+            this.Hiddentxt3.Name = "Hiddentxt3";
+            this.Hiddentxt3.ReadOnly = true;
+            this.Hiddentxt3.Size = new System.Drawing.Size(100, 20);
+            this.Hiddentxt3.TabIndex = 5;
+            this.Hiddentxt3.Visible = false;
+            // 
+            // Hiddentxt4
+            // 
+            this.Hiddentxt4.Location = new System.Drawing.Point(327, 106);
+            this.Hiddentxt4.Name = "Hiddentxt4";
+            this.Hiddentxt4.ReadOnly = true;
+            this.Hiddentxt4.Size = new System.Drawing.Size(100, 20);
+            this.Hiddentxt4.TabIndex = 6;
+            this.Hiddentxt4.Visible = false;
+            // 
+            // Hiddentxt5
+            // 
+            this.Hiddentxt5.Location = new System.Drawing.Point(434, 106);
+            this.Hiddentxt5.Name = "Hiddentxt5";
+            this.Hiddentxt5.ReadOnly = true;
+            this.Hiddentxt5.Size = new System.Drawing.Size(100, 20);
+            this.Hiddentxt5.TabIndex = 7;
+            this.Hiddentxt5.Visible = false;
+            // 
+            // AnswerBox1
+            // 
+            this.AnswerBox1.Location = new System.Drawing.Point(9, 150);
+            this.AnswerBox1.Name = "AnswerBox1";
+            this.AnswerBox1.Size = new System.Drawing.Size(100, 20);
+            this.AnswerBox1.TabIndex = 9;
+            // 
+            // AnswerBox2
+            // 
+            this.AnswerBox2.Location = new System.Drawing.Point(116, 149);
+            this.AnswerBox2.Name = "AnswerBox2";
+            this.AnswerBox2.Size = new System.Drawing.Size(100, 20);
+            this.AnswerBox2.TabIndex = 10;
+            // 
+            // AnswerBox3
+            // 
+            this.AnswerBox3.Location = new System.Drawing.Point(223, 149);
+            this.AnswerBox3.Name = "AnswerBox3";
+            this.AnswerBox3.Size = new System.Drawing.Size(100, 20);
+            this.AnswerBox3.TabIndex = 11;
+            // 
+            // AnswerBox4
+            // 
+            this.AnswerBox4.Location = new System.Drawing.Point(330, 150);
+            this.AnswerBox4.Name = "AnswerBox4";
+            this.AnswerBox4.Size = new System.Drawing.Size(100, 20);
+            this.AnswerBox4.TabIndex = 12;
+            // 
+            // AnswerBox5
+            // 
+            this.AnswerBox5.Location = new System.Drawing.Point(434, 149);
+            this.AnswerBox5.Name = "AnswerBox5";
+            this.AnswerBox5.Size = new System.Drawing.Size(100, 20);
+            this.AnswerBox5.TabIndex = 13;
+            // 
+            // TopicMenu
+            // 
+            this.TopicMenu.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.TopicMenu.FormattingEnabled = true;
+            this.TopicMenu.Items.AddRange(new object[] {
+            "Nintendo",
+            "Planet",
+            "Number"});
+            this.TopicMenu.Location = new System.Drawing.Point(211, 47);
+            this.TopicMenu.Name = "TopicMenu";
+            this.TopicMenu.Size = new System.Drawing.Size(121, 21);
+            this.TopicMenu.TabIndex = 14;
+            this.TopicMenu.SelectedIndexChanged += new System.EventHandler(this.TopicMenu_SelectedIndexChanged);
+            // 
+            // Lbl1
+            // 
+            this.Lbl1.AutoSize = true;
+            this.Lbl1.Location = new System.Drawing.Point(72, 79);
+            this.Lbl1.Name = "Lbl1";
+            this.Lbl1.Size = new System.Drawing.Size(0, 13);
+            this.Lbl1.TabIndex = 15;
+            // 
+            // a1
+            // 
+            this.a1.AutoSize = true;
+            this.a1.Location = new System.Drawing.Point(34, 191);
+            this.a1.Name = "a1";
+            this.a1.Size = new System.Drawing.Size(11, 13);
+            this.a1.TabIndex = 16;
+            this.a1.Text = "*";
+            this.a1.Visible = false;
+            // 
+            // aa2
+            // 
+            this.aa2.AutoSize = true;
+            this.aa2.Location = new System.Drawing.Point(145, 191);
+            this.aa2.Name = "aa2";
+            this.aa2.Size = new System.Drawing.Size(11, 13);
+            this.aa2.TabIndex = 17;
+            this.aa2.Text = "*";
+            this.aa2.Visible = false;
+            // 
+            // a3
+            // 
+            this.a3.AutoSize = true;
+            this.a3.Location = new System.Drawing.Point(252, 191);
+            this.a3.Name = "a3";
+            this.a3.Size = new System.Drawing.Size(11, 13);
+            this.a3.TabIndex = 18;
+            this.a3.Text = "*";
+            this.a3.Visible = false;
+            // 
+            // a4
+            // 
+            this.a4.AutoSize = true;
+            this.a4.Location = new System.Drawing.Point(361, 191);
+            this.a4.Name = "a4";
+            this.a4.Size = new System.Drawing.Size(11, 13);
+            this.a4.TabIndex = 19;
+            this.a4.Text = "*";
+            this.a4.Visible = false;
+            // 
+            // a5
+            // 
+            this.a5.AutoSize = true;
+            this.a5.Location = new System.Drawing.Point(456, 191);
+            this.a5.Name = "a5";
+            this.a5.Size = new System.Drawing.Size(11, 13);
+            this.a5.TabIndex = 20;
+            this.a5.Text = "*";
+            this.a5.Visible = false;
+            this.a5.Click += new System.EventHandler(this.a5_Click);
+            // 
+            // WinLbl
+            // 
+            this.WinLbl.AutoSize = true;
+            this.WinLbl.Location = new System.Drawing.Point(574, 191);
+            this.WinLbl.Name = "WinLbl";
+            this.WinLbl.Size = new System.Drawing.Size(0, 13);
+            this.WinLbl.TabIndex = 21;
+            // 
+            // TopicLbl
+            // 
+            this.TopicLbl.AutoSize = true;
+            this.TopicLbl.Location = new System.Drawing.Point(91, 52);
+            this.TopicLbl.Name = "TopicLbl";
+            this.TopicLbl.Size = new System.Drawing.Size(97, 13);
+            this.TopicLbl.TabIndex = 22;
+            this.TopicLbl.Text = "Please pick a topic";
+            // 
+            // amountCorrectLbl
+            // 
+            this.amountCorrectLbl.AutoSize = true;
+            this.amountCorrectLbl.Location = new System.Drawing.Point(432, 191);
+            this.amountCorrectLbl.Name = "amountCorrectLbl";
+            this.amountCorrectLbl.Size = new System.Drawing.Size(0, 13);
+            this.amountCorrectLbl.TabIndex = 23;
+            // 
+            // a2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(680, 267);
+            this.Controls.Add(this.amountCorrectLbl);
+            this.Controls.Add(this.TopicLbl);
+            this.Controls.Add(this.WinLbl);
+            this.Controls.Add(this.a5);
+            this.Controls.Add(this.a4);
+            this.Controls.Add(this.a3);
+            this.Controls.Add(this.aa2);
+            this.Controls.Add(this.a1);
+            this.Controls.Add(this.Lbl1);
+            this.Controls.Add(this.TopicMenu);
+            this.Controls.Add(this.AnswerBox5);
+            this.Controls.Add(this.AnswerBox4);
+            this.Controls.Add(this.AnswerBox3);
+            this.Controls.Add(this.AnswerBox2);
+            this.Controls.Add(this.AnswerBox1);
+            this.Controls.Add(this.Hiddentxt5);
+            this.Controls.Add(this.Hiddentxt4);
+            this.Controls.Add(this.Hiddentxt3);
+            this.Controls.Add(this.Hiddentxt2);
+            this.Controls.Add(this.Hiddentxt1);
+            this.Controls.Add(this.CheckBt);
+            this.Controls.Add(this.HideBt);
+            this.Controls.Add(this.LoadBt);
+            this.Name = "a2";
+            this.Text = "Hangman";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button LoadBt;
+        private System.Windows.Forms.Button HideBt;
+        private System.Windows.Forms.Button CheckBt;
+        private System.Windows.Forms.TextBox Hiddentxt1;
+        private System.Windows.Forms.TextBox Hiddentxt2;
+        private System.Windows.Forms.TextBox Hiddentxt3;
+        private System.Windows.Forms.TextBox Hiddentxt4;
+        private System.Windows.Forms.TextBox Hiddentxt5;
+        private System.Windows.Forms.TextBox AnswerBox1;
+        private System.Windows.Forms.TextBox AnswerBox2;
+        private System.Windows.Forms.TextBox AnswerBox3;
+        private System.Windows.Forms.TextBox AnswerBox4;
+        private System.Windows.Forms.TextBox AnswerBox5;
+        private System.Windows.Forms.ComboBox TopicMenu;
+        private System.Windows.Forms.Label Lbl1;
+        private System.Windows.Forms.Label a1;
+        private System.Windows.Forms.Label aa2;
+        private System.Windows.Forms.Label a3;
+        private System.Windows.Forms.Label a4;
+        private System.Windows.Forms.Label a5;
+        private System.Windows.Forms.Label WinLbl;
+        private System.Windows.Forms.Label TopicLbl;
+        private System.Windows.Forms.Label amountCorrectLbl;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Console;
+
+namespace HangManGame
+{
+    public partial class a2 : Form
+    {
+        public a2()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadBt_Click(object sender, EventArgs e)
+        {
+           //These are the strings and bools for the topics and word/answers
+            bool Nintendo;
+            bool Number;
+            bool Planet;
+
+            string NintendoTopic = "Nintendo";
+            string NumberTopic = "Number";
+            string PlanetTopic = "Planet";
+
+            
+            //This code sets the word into the text boxes if the player clicks load which the chosen topic
+            if (TopicMenu.Text == NintendoTopic)
+            {
+                string NintendString = "Mario";
+                Nintendo = true;
+
+                Char[] array = NintendString.ToCharArray();
+
+                Hiddentxt1.Text = array[0].ToString();
+                Hiddentxt2.Text = array[1].ToString();
+                Hiddentxt3.Text = array[2].ToString();
+                Hiddentxt4.Text = array[3].ToString();
+                Hiddentxt5.Text = array[4].ToString();
+
+               
+            }
+            else if (TopicMenu.Text == NumberTopic)
+            {
+                string NumberString = "Eight";
+                Number = true;
+
+                Char[] array = NumberString.ToCharArray();
+                
+                Hiddentxt1.Text = array[0].ToString();
+                Hiddentxt2.Text = array[1].ToString();
+                Hiddentxt3.Text = array[2].ToString();
+                Hiddentxt4.Text = array[3].ToString();
+                Hiddentxt5.Text = array[4].ToString();
+
+            }
+
+            else if (TopicMenu.Text == PlanetTopic){ 
+           
+                string PlanetString = "Earth";
+                Planet = true;
+
+                Char[] array = PlanetString.ToCharArray();
+
+                Hiddentxt1.Text = array[0].ToString();
+                Hiddentxt2.Text = array[1].ToString();
+                Hiddentxt3.Text = array[2].ToString();
+                Hiddentxt4.Text = array[3].ToString();
+                Hiddentxt5.Text = array[4].ToString();
+            }
+        }
+
+        private void CheckBt_Click(object sender, EventArgs e)
+        {
+            // these bools are to indicate whether the answer lbls are correct or not
+            bool correct = false;
+            bool ans1 = false;
+            bool ans2 = false;
+            bool ans3 = false;
+            bool ans4 = false;
+            bool ans5 = false;
+
+            //to calculate how many letters are correct
+            int amountCorrect = 0;
+            string amountCorrectStr;
+
+           //The player can only enter one charater per box so this is to help them from entering more than one
+
+            if (AnswerBox1.Text.Length > 1 || AnswerBox2.Text.Length >1 || AnswerBox3.Text.Length > 1 || AnswerBox4.Text.Length > 1 || AnswerBox5.Text.Length > 1)
+            {
+                Lbl1.Text = ("you can only enter one charater per box");
+               
+            }
+            
+            // to change the players input text into the string sensitive text, doesnt work for some reason
+
+            AnswerBox1.Text.ToUpper();
+            AnswerBox2.Text.ToLower();
+            AnswerBox3.Text.ToLower();
+            AnswerBox4.Text.ToLower();
+            AnswerBox5.Text.ToLower();
+             
+
+            //This section displays if the letter is correct underneath the playerintput boxes
+            if ( Hiddentxt1.Text == AnswerBox1.Text)
+            {
+                a1.Text = ("correct");
+                ans1 = true;
+            }
+            else { a1.Text = ("incorrect"); }
+            
+            if ( Hiddentxt2.Text == AnswerBox2.Text)
+            {
+                aa2.Text = ("correct");
+                ans2 = true;
+            }
+            else { aa2.Text = ("incorrect"); }
+            
+            if (Hiddentxt3.Text == AnswerBox3.Text)
+            {
+                a3.Text = ("correct");
+                ans3 = true;
+            }
+            else { a3.Text = ("incorrect"); }
+            
+            if (Hiddentxt4.Text == AnswerBox4.Text)
+            {
+                a4.Text = ("correct");
+                ans4 = true;
+            }
+            else { a4.Text = ("incorrect"); }
+
+            
+            if (Hiddentxt5.Text == AnswerBox5.Text)
+            {
+                a5.Text = ("correct");
+                ans5 = true;
+            }
+            else { a5.Text = ("incorrect"); }
+
+            if ( ans1 && ans2 && ans3 && ans4 && ans5 == true)
+            {
+                WinLbl.Text = ("You have won");
+            }
+            
+            if(a1.Text == "correct")
+            {
+                amountCorrect++;
+            }
+            if (aa2.Text == "correct")
+            {
+                amountCorrect++;
+            }
+            if (a3.Text == "correct")
+            {
+                amountCorrect++;
+            }
+            if (a4.Text == "correct")
+            {
+                amountCorrect++;
+            }
+            if (a5.Text == "correct")
+            {
+                amountCorrect++;
+            }
+
+            amountCorrectStr = Convert.ToString(amountCorrect);
+
+            amountCorrectLbl.Text = ("Out of 5 words you have " + amountCorrectStr + "correct");
+        }
+
+        
+
+        private void a5_Click(object sender, EventArgs e)
+        { }
+
+        private void HideBt_Click(object sender, EventArgs e)
+        {
+            //hide word button is toggle-able so that you can toggle the boxes from visible to invisible or visa versa
+            
+            if (Hiddentxt1.Visible &&  Hiddentxt2.Visible && Hiddentxt3.Visible && Hiddentxt4.Visible && Hiddentxt5.Visible ) 
+            { Hiddentxt1.Visible = false; Hiddentxt2.Visible = false; Hiddentxt3.Visible = false; Hiddentxt4.Visible = false; Hiddentxt5.Visible = false; }
+            else { Hiddentxt1.Visible = true; Hiddentxt2.Visible = true; Hiddentxt3.Visible = true; Hiddentxt4.Visible = true; Hiddentxt5.Visible = true; }
+
+            if (Hiddentxt1.Visible is false)
+            {
+                HideBt.Text = ("Show hint");
+            }
+            else 
+            {
+                HideBt.Text = ("Hide hint");
+                    }
+            
+            
+        }
+
+        private void TopicMenu_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // *unnessesary* On selection of topic boxes go invisible so that the word will not be seen when load word is clicked
+            
+            //if (Hiddentxt1.Visible && Hiddentxt2.Visible && Hiddentxt3.Visible && Hiddentxt4.Visible && Hiddentxt5.Visible)
+            //{ Hiddentxt1.Visible = false; Hiddentxt2.Visible = false; Hiddentxt3.Visible = false; Hiddentxt4.Visible = false; Hiddentxt5.Visible = false; }
+
+            
+           
+        }
+    }
+}
+
+//Planet Earth
+//Number Eight
+//Nintendo Mario
+/**
+                foreach (char value in array)
+               {
+                 Write( "{0}, {1}, {2}, {3}, {4}", Hiddentxt1, Hiddentxt2, Hiddentxt3, Hiddentxt4, Hiddentxt5);
+
+               }
+               
+               WriteLine (array, "{0}, {1}, {2}, {3}, {4}", Hiddentxt1, Hiddentxt2, Hiddentxt3, Hiddentxt4, Hiddentxt5);
+
+              
+            if (Hiddentxt1.Visible,  Hiddentxt2.Visible , Hiddentxt3.Visible , Hiddentxt4.Visible , Hiddentxt5.Visible ) 
+            { Hiddentxt1.Visible = false && Hiddentxt2.Visible = false && Hiddentxt3.Visible = false&& Hiddentxt4.Visible = false && Hiddentxt5.Visible = false; }
+            else { Hiddentxt1.Visible = true && Hiddentxt2.Visible = true&& Hiddentxt3.Visible = true&& Hiddentxt4.Visible = true&& Hiddentxt5.Visible = true; } 
+
+ **/
